d01abb3e94d117eaf14e8a8a0931e85f3a921bb2
Austin Morris <austin.morris@gmail.com>
2017-06-11 08:45:53 -0400

    basic pack/unpack

--- lib/velocy_stream.ex
+++ lib/velocy_stream.ex
@@ -1,18 +1,13 @@
 defmodule VelocyStream do
-  @moduledoc """
-  Documentation for VelocyStream.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  def pack(message, message_id \\ 0) when is_integer(message_id) do
+    length = IO.iodata_length(message) + 16
+    chunks = <<3, 0, 0, 0>>
+    [<<length::little-unsigned-size(32)>>, chunks, <<message_id::size(64)>>, message]
+  end
 
-  ## Examples
-
-      iex> VelocyStream.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def unpack(<<_length::little-unsigned-size(32), _chunks::size(32), message_id::size(64), message::binary>>) do
+    {message, message_id}
   end
 end
