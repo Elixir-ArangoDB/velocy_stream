86f31200103b963808e9bdaa66684ab70e5d8c66
Austin Morris <austin.morris@gmail.com>
2017-06-11 08:40:28 -0400

    initial project setup

---
+++ lib/velocy_stream.ex
@@ -0,0 +1,18 @@
+defmodule VelocyStream do
+  @moduledoc """
+  Documentation for VelocyStream.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VelocyStream.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
